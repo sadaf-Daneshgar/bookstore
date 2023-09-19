2239672adbfe81639a239d832f57fa1324b29aa8
“sadaf <sadaf.batekzai54321@gmial.com>
2023-09-19 23:44:32 +0430

    Fix the problem of category li

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import BookDiv from './components/BookForm';
 import BookList from './components/BookList';
 import Category from './components/category';
+import BookDiv from './components/BookForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Routes>
-        <Route path="/" element={<BookList />} />
+        <Route
+          path="/"
+          element={(
+            <>
+              <BookList />
+              <BookDiv />
+            </>
+          )}
+        />
         <Route path="/category" element={<Category />} />
       </Routes>
-      <BookDiv />
     </div>
   );
 }
